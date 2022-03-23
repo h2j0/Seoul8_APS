2d0c0a2005a80189518ac210f9589214c89eb644
dowoonlee <dwlee717@gmail.com>
2022-03-23 16:34:32 +0900

    Update boj1436.java

--- 이도운/boj/src/week08/boj1436.java
+++ 이도운/boj/src/week08/boj1436.java
@@ -3,40 +3,35 @@ package week08;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class boj1436 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		Set<Integer> set = new HashSet<Integer>();
+		
 
 		int n = Integer.parseInt(br.readLine());
+		String six = "666";
 
-		int d = 4;
-		int dd = 20;
-
-		while (dd <= n) {
-			d += 1;
-			int temp1 = 1;
-			for (int i = 0; i < (d - 4) * 10; i++) {
-				temp1 *= 9;
+		for (int digit = 4; digit < 8; digit++) {
+			for (int pwr = 0; pwr < (int) Math.pow(10, digit - 3); pwr++) {
+				String strPwr = String.format("%05d", pwr);
+				for (int btw = 0; btw <= strPwr.length(); btw++) {
+					int movieName = Integer.parseInt((strPwr.substring(0, btw)+six+strPwr.substring(btw, strPwr.length())));
+					set.add(movieName);
+				}
 			}
-			int temp2 = 1;
-			for (int i = 0; i < d - 3; i++) {
-				temp2 *= 10;
-			}
-			dd += (d - 3) * temp1 + temp2;
 		}
-
-		int n0;
-		int temp1 = 1;
-		int temp2 = 1;
-		for (int i = 0; i < (d - 4) * 10; i++) {
-			temp1 *= 9;
-		}
-		for (int i = 0; i < d - 3; i++) {
-			temp2 *= 10;
-		}
-		n0 = n-(dd- (d-3)*temp1 - temp2);
+		Integer[] array = new Integer[set.size()];
+		set.toArray(array);
+		Arrays.sort(array);
+		System.out.println(array[n-1]);
 
 	}
 
